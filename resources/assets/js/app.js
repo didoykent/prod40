aae6f888461f8a403c18554671015114bbbcaf5a
didoykent <XeniaAim@yahoo.com>
2018-08-29 18:40:46 +0900

    new commit:

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -23,14 +23,14 @@ import Quasar, { QLayout, QInput, QBtn, QChatMessage, QPage, QPageContainer, QTo
 
 
 QLayoutDrawer, QLayoutFooter, QScrollArea, QListHeader, QItem, QItemSide, QItemMain, QList,  QItemSeparator, QItemTile,
-QUploader, QScrollObservable, scroll, QField, QChipsInput, QChip, QPopover, CloseOverlay, QSpinnerDots, QInnerLoading, QSpinnerGears, QCard, QCardSeparator, QSpinner, QVideo
+QUploader, QScrollObservable, scroll, QField, QChipsInput, QChip, QPopover, CloseOverlay, QSpinnerDots, QInnerLoading, QSpinnerGears, QCard, QCardSeparator, QSpinner, QVideo, Platform
  } from "quasar-framework/dist/quasar.mat.esm";
 
  import QDrawer from "quasar-framework/dist/umd/quasar.ios.css";
 Vue.use(Quasar, { components: [QLayout, QInput, QBtn, QChatMessage, QPage, QPageContainer, QToolbar, QToolbarTitle, QLayoutHeader,
 
 QLayoutDrawer, QDrawer, QLayoutFooter, QScrollArea, QListHeader, QItem, QItemSide, QItemMain, QList, QItemSeparator, QItemTile, QUploader,
- QScrollObservable, scroll, QField, QChipsInput, QChip, QPopover, CloseOverlay, QSpinnerDots, QInnerLoading, QSpinnerGears, QCard, QCardSeparator, QSpinner, QVideo
+ QScrollObservable, scroll, QField, QChipsInput, QChip, QPopover, CloseOverlay, QSpinnerDots, QInnerLoading, QSpinnerGears, QCard, QCardSeparator, QSpinner, QVideo, Platform
 
 
 ] });
